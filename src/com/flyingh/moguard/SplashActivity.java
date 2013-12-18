2a94b5c9bf6bc9e0c18765923d76fa85f3780a2a
Flycoding <Flycoding@yeah.net>
2013-12-18 20:17:34 +0800

    implement the splash activity.

---
+++ src/com/flyingh/moguard/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.flyingh.moguard;
+
+import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class SplashActivity extends Activity {
+	private static final String TAG = "SplashActivity";
+	private TextView versionNameTextView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		versionNameTextView = (TextView) findViewById(R.id.version_name);
+		try {
+			PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+			versionNameTextView.setText(packageInfo.versionName);
+		} catch (NameNotFoundException e) {
+			Log.i(TAG, e.getMessage());
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
