585d57c6564435301ce51c0140f672d545edab63
Flycoding <Flycoding@yeah.net>
2013-12-23 21:30:24 +0800

    select button selector drawable.

--- gen/com/flyingh/moguard/R.java
+++ gen/com/flyingh/moguard/R.java
@@ -23,18 +23,21 @@ public final class R {
     public static final class drawable {
         public static final int app3=0x7f020000;
         public static final int atools8=0x7f020001;
-        public static final int callmsgsafe2=0x7f020002;
-        public static final int dialog=0x7f020003;
-        public static final int dialog_title=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int icon=0x7f020006;
-        public static final int logo=0x7f020007;
-        public static final int netmanager6=0x7f020008;
-        public static final int security=0x7f020009;
-        public static final int settings9=0x7f02000a;
-        public static final int sysoptimize7=0x7f02000b;
-        public static final int taskmanager4=0x7f02000c;
-        public static final int trojan5=0x7f02000d;
+        public static final int button_not_pressed_shape=0x7f020002;
+        public static final int button_pressed_shape=0x7f020003;
+        public static final int button_selector=0x7f020004;
+        public static final int callmsgsafe2=0x7f020005;
+        public static final int dialog=0x7f020006;
+        public static final int dialog_title=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int icon=0x7f020009;
+        public static final int logo=0x7f02000a;
+        public static final int netmanager6=0x7f02000b;
+        public static final int security=0x7f02000c;
+        public static final int settings9=0x7f02000d;
+        public static final int sysoptimize7=0x7f02000e;
+        public static final int taskmanager4=0x7f02000f;
+        public static final int trojan5=0x7f020010;
     }
     public static final class id {
         public static final int action_settings=0x7f08000b;
