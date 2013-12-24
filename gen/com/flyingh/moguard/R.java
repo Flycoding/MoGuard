d17b872141cd85e272ab78329479c2273a6e17c6
Flycoding <Flycoding@yeah.net>
2013-12-24 22:20:07 +0800

    complete the last 3 UI.

--- gen/com/flyingh/moguard/R.java
+++ gen/com/flyingh/moguard/R.java
@@ -23,37 +23,42 @@ public final class R {
     public static final class drawable {
         public static final int app3=0x7f020000;
         public static final int atools8=0x7f020001;
-        public static final int button_not_pressed_shape=0x7f020002;
-        public static final int button_pressed_shape=0x7f020003;
-        public static final int button_selector=0x7f020004;
-        public static final int callmsgsafe2=0x7f020005;
-        public static final int dialog=0x7f020006;
-        public static final int dialog_title=0x7f020007;
-        public static final int divide_line=0x7f020008;
-        public static final int ic_launcher=0x7f020009;
-        public static final int icon=0x7f02000a;
-        public static final int logo=0x7f02000b;
-        public static final int netmanager6=0x7f02000c;
-        public static final int next=0x7f02000d;
-        public static final int security=0x7f02000e;
-        public static final int settings9=0x7f02000f;
-        public static final int setup1=0x7f020010;
-        public static final int sysoptimize7=0x7f020011;
-        public static final int taskmanager4=0x7f020012;
-        public static final int trojan5=0x7f020013;
+        public static final int bind=0x7f020002;
+        public static final int button_not_pressed_shape=0x7f020003;
+        public static final int button_pressed_shape=0x7f020004;
+        public static final int button_selector=0x7f020005;
+        public static final int callmsgsafe2=0x7f020006;
+        public static final int dialog=0x7f020007;
+        public static final int dialog_title=0x7f020008;
+        public static final int divide_line=0x7f020009;
+        public static final int ic_launcher=0x7f02000a;
+        public static final int icon=0x7f02000b;
+        public static final int logo=0x7f02000c;
+        public static final int netmanager6=0x7f02000d;
+        public static final int next=0x7f02000e;
+        public static final int ok=0x7f02000f;
+        public static final int phone=0x7f020010;
+        public static final int previous=0x7f020011;
+        public static final int security=0x7f020012;
+        public static final int settings9=0x7f020013;
+        public static final int setup1=0x7f020014;
+        public static final int sysoptimize7=0x7f020015;
+        public static final int taskmanager4=0x7f020016;
+        public static final int trojan5=0x7f020017;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000b;
+        public static final int action_settings=0x7f08000c;
+        public static final int binding_phone_number=0x7f080007;
         public static final int cancel_input_password=0x7f080005;
-        public static final int cancel_set_password=0x7f080009;
+        public static final int cancel_set_password=0x7f08000a;
         public static final int confirm_input_password=0x7f080006;
-        public static final int confirm_set_password=0x7f08000a;
+        public static final int confirm_set_password=0x7f08000b;
         public static final int feature_name=0x7f080003;
         public static final int grid_view=0x7f080000;
         public static final int input_password=0x7f080004;
         public static final int linear_layout=0x7f080001;
-        public static final int password=0x7f080007;
-        public static final int repeat_password=0x7f080008;
+        public static final int password=0x7f080008;
+        public static final int repeat_password=0x7f080009;
         public static final int version_name=0x7f080002;
     }
     public static final class layout {
@@ -64,7 +69,10 @@ public final class R {
         public static final int grid_view_item=0x7f030004;
         public static final int input_security_password_dialog_view=0x7f030005;
         public static final int security_wizard=0x7f030006;
-        public static final int set_security_password_dialog_view=0x7f030007;
+        public static final int security_wizard2=0x7f030007;
+        public static final int security_wizard3=0x7f030008;
+        public static final int security_wizard4=0x7f030009;
+        public static final int set_security_password_dialog_view=0x7f03000a;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -75,9 +83,11 @@ public final class R {
         public static final int action_settings=0x7f040002;
         public static final int advanced_tools=0x7f040011;
         public static final int alarm_when_sim_changed=0x7f040028;
+        public static final int alarm_will_ring_at_boot_time_when_sim_card_changed=0x7f040030;
         public static final int app_name=0x7f040001;
         public static final int apps=0x7f04000c;
         public static final int are_you_sure_to_cancel_the_download_=0x7f04001b;
+        public static final int bind=0x7f040034;
         public static final int cancel=0x7f040014;
         public static final int cancel_=0x7f04001c;
         public static final int canceled=0x7f040007;
@@ -87,6 +97,7 @@ public final class R {
         public static final int divide_line=0x7f040026;
         public static final int downloading_=0x7f040006;
         public static final int first_process_setup_wizard=0x7f040025;
+        public static final int fourth_process_setup_wizard=0x7f040033;
         public static final int get_update_information_failed=0x7f040004;
         public static final int gps_tracker=0x7f040029;
         public static final int hello_world=0x7f040003;
@@ -98,15 +109,19 @@ public final class R {
         public static final int network_is_not_available_=0x7f040017;
         public static final int network_manager=0x7f04000f;
         public static final int next=0x7f04002d;
+        public static final int not_started=0x7f040039;
         public static final int ok=0x7f040015;
         public static final int password_is_wrong=0x7f040023;
         public static final int phone_guard=0x7f04000b;
         public static final int please_input_the_feature_s_new_name=0x7f040009;
+        public static final int previous=0x7f04002e;
         public static final int process=0x7f04002c;
         public static final int remote_erase_data=0x7f04002a;
         public static final int remote_lock_screen=0x7f04002b;
         public static final int repeat_password=0x7f040020;
+        public static final int second_process_setup_wizard=0x7f040031;
         public static final int security=0x7f04000a;
+        public static final int select=0x7f040037;
         public static final int set_password=0x7f04001e;
         public static final int settings=0x7f040012;
         public static final int system_optimize=0x7f040010;
@@ -114,16 +129,25 @@ public final class R {
         public static final int the_feature_name_should_not_be_empty_=0x7f040013;
         public static final int the_password_and_the_repeat_password_are_not_the_same=0x7f040021;
         public static final int the_password_or_the_repeat_password_should_not_be_empty=0x7f040022;
+        public static final int the_phone_will_send_sms_to_the_phone_number_when_sim_card_changed=0x7f040036;
+        public static final int third_process_setup_wizard=0x7f040032;
         public static final int title_activity_main=0x7f040005;
         public static final int title_activity_security=0x7f040008;
         public static final int title_activity_security_wizard=0x7f040024;
+        public static final int unbound=0x7f040035;
         public static final int update_info_url=0x7f040000;
-        public static final int welcome_to_use_security_setup_wizard=0x7f04002e;
+        public static final int welcome_to_use_security_setup_wizard=0x7f04002f;
+        public static final int you_had_better_launch_the_security=0x7f040038;
     }
     public static final class style {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
+
+
+
+
 
 
 
@@ -165,6 +189,11 @@ public final class R {
 
 
 
+
+
+
+
+
         
 
         Base application theme for API 11+. This theme completely replaces
@@ -184,12 +213,14 @@ public final class R {
          */
         public static final int AppTheme=0x7f060001;
         public static final int SecurityDialogTheme=0x7f060002;
-        public static final int btn_next=0x7f060008;
+        public static final int btn_next=0x7f060009;
+        public static final int btn_previous=0x7f06000a;
+        public static final int btn_previous_next=0x7f060008;
         public static final int divide_line=0x7f060004;
         public static final int presence_offline=0x7f060007;
         public static final int presence_online=0x7f060006;
         public static final int security_wizard_feature=0x7f060005;
         public static final int setup_wizard_process_title=0x7f060003;
-        public static final int setup_wizard_title=0x7f060009;
+        public static final int setup_wizard_title=0x7f06000b;
     }
 }
